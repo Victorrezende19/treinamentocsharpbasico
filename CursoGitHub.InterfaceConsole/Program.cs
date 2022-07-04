cf8426a49d338216e49b84cdb54cecea8db6cc82
Victorrezende19 <victor.pr99@hotmail.com>
2022-07-04 11:00:54 -0300

    first commit

---
+++ CursoGitHub.InterfaceConsole/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoGitHub.InterfaceConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {//inicio do metodo
+            /* Main - principal
+             * o metodo inicia-se com a letra maiuscula
+             * 
+             * 
+             */
+            Console.WriteLine("alo mundo");
+            Console.ReadKey();
+        }
+    }
+}
